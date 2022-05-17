3fc9d56f26bd7bccd67760408b0bb901859a6747
Austin Mac <austinmac@ucsb.edu>
2022-05-17 13:48:49 -0700

    am - page for recommendation table

--- frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
+++ frontend/src/main/pages/Recommendations/RecommendationsIndexPage.js
@@ -12,9 +12,9 @@ export default function RecommendationsIndexPage() {
   const { data: recommendations, error: _error, status: _status } =
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/ucsbrecommendations/all"],
+      ["/api/Recommendation/all"],
             // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/ucsbrecommendations/all" },
+            { method: "GET", url: "/api/Recommendation/all" },
       []
     );
 
