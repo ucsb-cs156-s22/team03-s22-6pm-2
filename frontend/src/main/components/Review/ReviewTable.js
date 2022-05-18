de5d678641bf0b948fde97b33165dfcdfd4e2289
Sid Majeti <sid@169-231-155-141.wireless.ucsb.edu>
2022-05-17 23:43:14 -0700

    added table to review index page

---
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -0,0 +1,77 @@
+import OurTable, { _ButtonColumn} from "main/components/OurTable";
+// import { useBackendMutation } from "main/utils/useBackend";
+// import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+// import { hasRole } from "main/utils/currentUser";
+
+
+// export function cellToAxiosParamsDelete(cell) {
+//     return {
+//         url: "/api/ucsbdiningcommons",
+//         method: "DELETE",
+//         params: {
+//             code: cell.row.values.code
+//         }
+//     }
+// }
+
+export default function ReviewTable({ review, _currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/ucsbdiningcommons/all"]
+    // );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'itemID',
+            accessor: 'itemId', 
+        },
+        {
+            Header: 'reviewerEmail',
+            accessor: 'reviewerEmail',
+        },
+        {
+            Header: 'stars',
+            accessor: 'stars', // needed for tests
+        },
+        {
+            Header: 'date reviewed',
+            accessor: 'dateReviewed', // needed for tests
+
+        },
+        {
+            Header: 'comments?',
+            accessor: 'comments', // needed for tests
+        },
+    ];
+
+    // const testid = "ReviewTable";
+
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     // ButtonColumn("Edit", "primary", editCallback, testid),
+    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
+    // ];
+
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = columns;
+
+    return <OurTable
+        data={review}
+        columns={columnsToDisplay}
+        testid={"ReviewTable"}
+    />;
+};
--- frontend/src/main/pages/Review/ReviewIndexPage.js
+++ frontend/src/main/pages/Review/ReviewIndexPage.js
@@ -1,14 +1,28 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+import ReviewTable from 'main/components/Review/ReviewTable';
 
 export default function ReviewIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: review, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/MenuItemReview/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/MenuItemReview/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Review </h1>
-        <p>
-            This is where the index page will go
-        </p>
+        <h1>Review</h1>
+        <ReviewTable review={review} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
