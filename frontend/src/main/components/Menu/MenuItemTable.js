68370e840c3fb3c6a9c746e5152c570a0ac3dea5
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2022-05-18 16:30:14 -0700

    create menu item table

---
+++ frontend/src/main/components/Menu/MenuItemTable.js
@@ -0,0 +1,69 @@
+import OurTable, { ButtonColumn} from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/ucsbdiningcommonsmenuitem",
+        method: "DELETE",
+        params: {
+            code: cell.row.values.code
+        }
+    }
+}
+
+export default function MenuItemsCommonsTable({ menuItem, currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/ucsbdiningcommonsmenuitem/all"]
+    );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    {
+       
+      }
+    const columns = [
+        {
+            Header: 'Dining Commons Code',
+            accessor: 'diningCommonsCode', 
+        },
+        {
+            Header: 'Name',
+            accessor: 'name',
+        },
+        {
+            Header: 'Station',
+            accessor: 'station',
+        }
+    ];
+
+    const testid = "MenuItemTable";
+
+    const columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, testid),
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={menuItem}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
