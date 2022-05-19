e9d618673dceaa86a76c47285f079de35e45a049
Nirmit Ashar <13338417+nirmitashar@users.noreply.github.com>
2022-05-18 18:46:45 -0700

    adding testing for menu item table

---
+++ frontend/src/fixtures/menuItemFixtures.js
@@ -0,0 +1,29 @@
+const menuItemFixtures = {
+    oneMenuItem: 
+             {
+            "diningCommonsCode": "dlg",
+            "station": "test3",
+            "name": "tofu banh mi"
+        }
+,
+    threeMenuItems: [
+        {
+            "diningCommonsCode": "dlg",
+            "station": "test3",
+            "name": "tofu banh mi"
+        } ,
+        {
+            "diningCommonsCode": "ortega",
+            "station": "test2",
+            "name": "pizza"
+        } ,
+        {
+            "diningCommonsCode": "portola",
+            "station": "test1",
+            "name": "taco bell"
+        } 
+    ]
+};
+
+
+export { menuItemFixtures }; 
