a2843f29c77c4eba9462ed4d0ef2ae6710f86140
Austin Mac <austinmac@ucsb.edu>
2022-05-18 18:13:14 -0700

    am - add recommenation table

--- frontend/src/main/components/Recommendations/RecommendationsTable.js
+++ frontend/src/main/components/Recommendations/RecommendationsTable.js
@@ -10,7 +10,7 @@ export function cellToAxiosParamsDelete(cell) {
         url: "/api/Recommendation",
         method: "DELETE",
         params: {
-            id: cell.row.values.id
+            id: cell.row.values.id 
         }
     }
 }
