502eac0f5c71c11e0f553df042392632e394af0e
Michael Curtis <mikey1030@gmail.com>
2022-05-18 21:12:11 -0700

    wokring on organizationTable

--- frontend/src/main/pages/Organizations/OrganizationsIndexPage.js
+++ frontend/src/main/pages/Organizations/OrganizationsIndexPage.js
@@ -1,28 +1,27 @@
 import React from 'react'
-//import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
-
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-//import OrganizatinsTable from 'main/components/Organizations/Organizations';
+import OrganizationsTable from 'main/components/Organizations/OrganizationsTable';
 import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function OrganizationsIndexPage() {
 
-  const _currentUser = useCurrentUser();
+  const currentUser = useCurrentUser();
 
-  /*const { data: _organizations, error: _error, status: _status } =
+  const { data: organizations, error: _error, status: _status } =
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/ucsborganization/all"],
+      ["/api/UCSBOrganization/all"],
             // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/ucsborganization/all" },
+            { method: "GET", url: "/api/UCSBOrganization/all" },
       []
-    );*/
+    );
 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>UCSB Organizations Placeholder</h1>
-        {/*<DiningCommonsTable diningCommons={diningCommons} currentUser={currentUser} />*/}
+        <h1>UCSB Organizations</h1>
+        <OrganizationsTable organizations={organizations} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
