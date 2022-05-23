46d4b7438f2da8912627248479a50156cde96b5b
Sid Majeti <sid@169-231-159-30.wireless.ucsb.edu>
2022-05-23 14:04:36 -0700

    fix merge conflict issues with ReviewTable

--- frontend/src/main/components/Review/ReviewTable.js
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -1,26 +1,21 @@
-import OurTable, { ButtonColumn} from "main/components/OurTable";
+import OurTable, { _ButtonColumn} from "main/components/OurTable";
 // import { useBackendMutation } from "main/utils/useBackend";
 // import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
 // import { useNavigate } from "react-router-dom";
 // import { hasRole } from "main/utils/currentUser";
 
-import { useBackendMutation } from "main/utils/useBackend";
-import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
-// import { useNavigate } from "react-router-dom";
-import { hasRole } from "main/utils/currentUser";
 
+// export function cellToAxiosParamsDelete(cell) {
+//     return {
+//         url: "/api/ucsbdiningcommons",
+//         method: "DELETE",
+//         params: {
+//             code: cell.row.values.code
+//         }
+//     }
+// }
 
-export function cellToAxiosParamsDelete(cell) {
-    return {
-        url: "/api/MenuItemReview",
-        method: "DELETE",
-        params: {
-            id: cell.row.values.id
-        }
-    }
-}
-
-export default function ReviewTable({ review, currentUser }) {
+export default function ReviewTable({ review, _currentUser }) {
 
     // const navigate = useNavigate();
 
@@ -28,17 +23,16 @@ export default function ReviewTable({ review, currentUser }) {
     //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
     // }
 
-
-    // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteMutation = useBackendMutation(
-        cellToAxiosParamsDelete,
-        { onSuccess: onDeleteSuccess },
-        ["/api/MenuItemReview/all"]
-    );
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/ucsbdiningcommons/all"]
+    // );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -68,16 +62,16 @@ export default function ReviewTable({ review, currentUser }) {
         },
     ];
 
+    // const testid = "ReviewTable";
 
-    const testid = "ReviewTable";
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     // ButtonColumn("Edit", "primary", editCallback, testid),
+    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
+    // ];
 
-    const columnsIfAdmin = [
-        ...columns,
-        // ButtonColumn("Edit", "primary", editCallback, testid),
-        ButtonColumn("Delete", "danger", deleteCallback, testid)
-    ];
-
-    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = columns;
 
     return <OurTable
         data={review}
